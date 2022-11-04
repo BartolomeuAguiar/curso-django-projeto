6d46f557eb2bac8bddad26adea13e7a57729e1a6
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-03 22:05:54 -0300

    removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,11 +5,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
