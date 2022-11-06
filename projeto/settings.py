4dedfe8ca036da6241df57abd17ae884102d77e8
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-06 10:20:34 -0300

    finalizada aula 41. Configurada a pasta STATIC_ROOT.

--- projeto/settings.py
+++ projeto/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
