bfd621d35993db5b6f493188c612311a6037c834
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-06 18:02:47 -0300

    finalizada aula 46. criada factory de recipes. adaptada a página home para exivir a lista de recipes.

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(6)],
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
