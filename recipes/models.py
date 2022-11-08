ac7bae89596017a6c8f9fe4c3af051980ea6e5fc
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-07 21:17:40 -0300

    Fim da aula 51. criada a classe de recipes.

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,18 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
