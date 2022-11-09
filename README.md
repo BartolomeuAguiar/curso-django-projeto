97d10d85f75d9c6686eb0a269c1d0bc7d9861f27
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-08 23:13:46 -0300

    Finished lesson 59. Created the page of categories.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 ##### template da linguagem django
 https://docs.djangoproject.com/pt-br/4.0/ref/templates/language/
+
+#### Documentação Querysets
+https://docs.djangoproject.com/en/4.1/topics/db/queries/
+https://docs.djangoproject.com/pt-br/4.1/ref/models/querysets/
--- recipes/views.py
+++ recipes/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 # Create your views here.
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(6)],
-        'is_datail_page': False,
+        'recipes': recipes
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes
     })
 
 
