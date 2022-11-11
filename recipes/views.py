923af69a6c64ef1023d130383d7adebe6e1a3cb5
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-10 22:57:49 -0300

    Finalizada aula 65

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from utils.recipes.factory import make_recipe
 
 from .models import Recipe
 
 # Create your views here.
+
+# Exibe Todas as receitas em formato de lista
 
 
 def home(request):
@@ -14,6 +16,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes
     })
+
+# Exibe as Receitas Filtradas pela Categoria Selecionada
 
 
 def category(request, category_id):
@@ -31,9 +35,18 @@ def category(request, category_id):
         'title': f'{category_name} - Category'
     })
 
+# Exibe  apenas Uma receita na página
+
 
 def recipe(request, id):
+    # recipe = Recipe.objects.filter(
+    #     id=id,
+    #     is_published=True,
+    # ).order_by('-id').first()
+    recipe = get_object_or_404(
+        Recipe.objects.filter(id=id, is_published=True))
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_datail_page': True,
     })
