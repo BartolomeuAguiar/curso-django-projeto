abc52d7f56e1e1e1e46f45729df3fcab3672fe56
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-12 15:54:08 -0300

    Finalizada aula 76

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from recipes import views
 
 # Create your tests here.
 
@@ -16,3 +18,17 @@ class RecipeURLsTest(TestCase):
     def test_recipe_url_is_correctly(self):
         url = reverse('recipes:recipe', kwargs={'id': 5})
         self.assertEqual(url, '/recipes/5/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_functions_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertTrue(view.func, views.home)
+
+    def test_recipe_category_view_functions_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertTrue(view.func, views.category)
+
+    def test_recipe_detail_view_functions_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertTrue(view.func, views.recipe)
