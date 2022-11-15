41a41a1535ad24823b36e3f2209f70090ea64e11
Antonio Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-14 23:03:20 -0300

    Finalizada aula 80

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -9,14 +9,6 @@ class RecipeViewsTest(TestCase):
         view = resolve(reverse('recipes:home'))
         self.assertTrue(view.func, views.home)
 
-    def test_recipe_category_view_functions_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertTrue(view.func, views.category)
-
-    def test_recipe_detail_view_functions_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertTrue(view.func, views.recipe)
-
     def test_recipe_home_view_returns_status_code_200_ok(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
@@ -24,3 +16,28 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>NADA PARA MOSTRAR AQUI 🐛</h1>',
+            response.content.decode('utf-8')
+        )
+
+    def test_recipe_category_view_functions_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertTrue(view.func, views.category)
+
+    def test_recipe_category_view_returns_404_if_not_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_view_functions_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertTrue(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_404_if_not_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000}))
+        self.assertEqual(response.status_code, 404)
