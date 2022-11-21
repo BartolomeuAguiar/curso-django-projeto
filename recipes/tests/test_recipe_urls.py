2b71b8539aab48ddfad0d299a6c6d9def8086d2d
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-21 12:07:06 -0400

    Finished lesson 99

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -14,3 +14,7 @@ class RecipeURLsTest(TestCase):
     def test_recipe_url_is_correctly(self):
         url = reverse('recipes:recipe', kwargs={'id': 5})
         self.assertEqual(url, '/recipes/5/')
+
+    def test_recipe_search_url_is_correct(self):
+        url = reverse('recipes:search')
+        self.assertEqual(url, '/recipes/search/')
