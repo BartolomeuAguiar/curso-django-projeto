2dc061b0fb6895355db789936bcb14cef51e54c4
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-23 20:48:26 -0400

    Finished lesson 119

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,9 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from utils.pagination import make_pagination_range
 
 from .models import Recipe
 
@@ -13,11 +16,24 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
-    # recipes = get_list_or_404(Recipe.objects.filter(
-    #         is_published = True,).order_by('-id')
-    #     )
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+
     })
 
 # Returns the Recipes filtereds by category
