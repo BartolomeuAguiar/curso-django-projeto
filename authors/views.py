3c768be5795b8491f92234f93b675b6deb3e875c
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-24 19:43:11 -0400

    Finished lesson 130

--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from .forms import RegisterForm
+
 # Create your views here.
 
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
