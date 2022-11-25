033bbf7022b0a8b40bb000f2792afdcddb69c487
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-25 19:35:35 -0400

    lesson 140

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -22,4 +23,9 @@ def register_create(request):
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)  # noqa : F841
 
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del (request.session['register_form_data'])
     return redirect('authors:register')
