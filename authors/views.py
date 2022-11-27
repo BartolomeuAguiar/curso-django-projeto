fe8a0bb0b8d0125e940bd08d8499d24bbec2c039
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-27 09:21:36 -0400

    lesson 150

--- authors/views.py
+++ authors/views.py
@@ -24,7 +24,9 @@ def register_create(request):
     form = RegisterForm(POST)  # noqa : F841
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user is created, please log in.')
 
         del (request.session['register_form_data'])
