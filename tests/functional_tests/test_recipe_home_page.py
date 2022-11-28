79420da6425fc5c0b30bd00a9c6e3243a0975b0a
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-11-28 19:13:32 -0400

    Finished lesson 164

---
+++ tests/functional_tests/test_recipe_home_page.py
@@ -0,0 +1,19 @@
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
+
+from utils.browser import make_chrome_browser
+
+
+class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
+    def sleep(self, seconds=5):
+        time.sleep(seconds)
+
+    def test_the_test(self):
+        browser = make_chrome_browser()
+        browser.get(self.live_server_url)
+        self.sleep(1)
+        body = browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('NO RECIPES FOUND HERE 🐛', body.text)
+        browser.quit()
