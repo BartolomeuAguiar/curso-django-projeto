90273f59e07a8c523476dad066be374a27c8d32f
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-12-04 13:11:11 -0400

    lesson 181

--- recipes/admin.py
+++ recipes/admin.py
@@ -11,7 +11,14 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'title', 'created_at', 'author', 'is_published']
+    list_display_links = ['id', 'title', 'created_at']
+    search_fields = ['id', 'title', 'slug', 'description', 'preparation_steps']
+    list_filter = ['category', 'author',
+                   'is_published', 'preparation_steps_is_html']
+    list_per_page = 10
+    list_editable = ['is_published', 'author']
+    ordering = ['-id']
 
 
 admin.site.register(Category, CategoryAdmin)
