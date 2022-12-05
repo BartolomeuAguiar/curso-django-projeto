84af07fc1a23cb3bdde2021888ee66d6d1cb96a5
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-12-05 17:31:01 -0400

    lesson 190

--- authors/views.py
+++ authors/views.py
@@ -127,3 +127,28 @@ def dashboard_recipe_edit(request, id):
                   context={
                       'form': form
                   })
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_new(request):
+
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        recipe.save()
+        messages.success(request, 'Your recipe are created succesfully')
+        return redirect(reverse('authors:dashboard_recipe_new'))
+
+    return render(request, 'authors/pages/dashboard_recipe.html',
+                  context={
+                      'form': form,
+                      'form_action': reverse('authors:dashboard_recipe_new'),
+                  })
