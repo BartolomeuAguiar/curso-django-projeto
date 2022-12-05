4c527b064822932ec53114058128d27d63e1c597
Bartolomeu Aguiar <bartolomeu.abnma@gmail.com>
2022-12-04 22:46:21 -0400

    lesson 188

--- authors/views.py
+++ authors/views.py
@@ -109,9 +109,19 @@ def dashboard_recipe_edit(request, id):
         raise Http404()
 
     form = AuthorRecipeForm(
-        request.POST or None,
+        data=request.POST or None,
+        files=request.FILES or None,
         instance=recipe
     )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        recipe.save()
+        messages.success(request, 'Sua receita foi salva com sucesso')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
 
     return render(request, 'authors/pages/dashboard_recipe.html',
                   context={
