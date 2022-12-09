61f256f963f51fe2fd93d590503b0ac544865aaa
Bartolomeu Aguiar <31780812+BartolomeuAguiar@users.noreply.github.com>
2022-12-08 21:40:32 -0400

    lesson 229. skiped sectios 17 and 18.

--- project/settings.py
+++ project/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     # project
     'recipes',
     'authors',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,3 +148,7 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'message-success',
     constants.WARNING: 'message-warning',
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
