d08558169afed82435ca4b94671b9c484795434b
Bartolomeu Aguiar <31780812+BartolomeuAguiar@users.noreply.github.com>
2022-12-13 22:31:15 -0400

    lesson 247

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,10 +5,24 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.RecipeListViewHome.as_view(), name="home"),
+    path('',
+         views.RecipeListViewHome.as_view(),
+         name="home"
+         ),
     path('recipes/search/',
-         views.RecipeListViewSearch.as_view(), name="search"),
+         views.RecipeListViewSearch.as_view(),
+         name="search"
+         ),
+    path('recipes/tags/<slug:slug>',
+         views.RecipeListViewTag.as_view(),
+         name="tag"
+         ),
     path('recipes/category/<int:category_id>/',
-         views.RecipeListViewCategory.as_view(), name="category"),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
+         views.RecipeListViewCategory.as_view(),
+         name="category"
+         ),
+    path('recipes/<int:pk>/',
+         views.RecipeDetail.as_view(),
+         name="recipe"
+         ),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,7 @@ from django.http.response import Http404
 from django.views.generic import DetailView, ListView
 
 from recipes.models import Recipe
+from tag.models import Tag
 from utils.pagination import make_pagination
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
@@ -22,6 +23,7 @@ class RecipeListViewBase(ListView):
             is_published=True,
         )
         qs = qs.select_related('author', 'category')
+        qs = qs.prefetch_related('tags')
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -110,6 +112,32 @@ class RecipeDetail(DetailView):
         ctx = super().get_context_data(*args, **kwargs)
         ctx.update({
             'is_detail_page': True
+        })
+
+        return ctx
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(tags__slug=self.kwargs.get('slug', ''))
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(
+            slug=self.kwargs.get('slug', '')).first()
+
+        if not page_title:
+            page_title = 'No recipes found'
+
+        page_title = f'{page_title} - Tag |'
+
+        ctx.update({
+            'page_title': f'Search for "{page_title}" |',
+
         })
 
         return ctx
