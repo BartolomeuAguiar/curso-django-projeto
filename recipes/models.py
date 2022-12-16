376a8548ddf59a0f0e8f54e4590d524aeb49a4c9
Bartolomeu Aguiar <31780812+BartolomeuAguiar@users.noreply.github.com>
2022-12-15 20:30:31 -0400

    lesson 255

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,9 @@
 # Create your models here.
+from collections import defaultdict
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.forms import ValidationError
 from django.urls import reverse
 from django.utils.text import slugify
 
@@ -47,3 +50,19 @@ class Recipe(models.Model):
             self.slug = slug
 
         return super().save(*args, **kwargs)
+
+    def clean(self, *args, **kwargs):
+        error_messages = defaultdict(list)
+
+        recipe_from_db = Recipe.objects.filter(
+            title__iexact=self.title
+        ).first()
+
+        if recipe_from_db:
+            if recipe_from_db.pk != self.pk:
+                error_messages['title'].append(
+                    'Found recipes with the same title.'
+                )
+
+        if error_messages:
+            raise ValidationError(error_messages)
