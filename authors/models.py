67b4a3d59381bc98904230eee15e92db4e27ff69
Bartolomeu Aguiar <31780812+BartolomeuAguiar@users.noreply.github.com>
2022-12-17 20:39:45 -0400

    lesson 261

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='', blank=True)
